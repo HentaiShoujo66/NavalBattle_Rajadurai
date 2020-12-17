f4d643915b50fe731a5016d9fbaa314f011052c2
HentaiShoujo66 <thirusan.rajadurai@cpnv.ch>
2020-12-17 16:01:35 +0100

    problèmes à régler

--- BatailleNavale/main.c
+++ BatailleNavale/main.c
@@ -15,8 +15,9 @@ int MenuChoice;
 int playercolumn;
 int playerline;
 int line_input_number;
-int sunk_boats=0;
+int sunk_boats = 0;
 int new_sunk_boat = 0;
+int score = 0;
 char column_input_letter;
 char grid[10][10];
 char coord_letter;
@@ -26,6 +27,11 @@ bool boat2_sunk = false;
 bool boat3_sunk = false;
 bool boat4_sunk = false;
 bool boat5_sunk = false;
+bool boat1_msg = false;
+bool boat2_msg = false;
+bool boat3_msg = false;
+bool boat4_msg = false;
+bool boat5_msg = false;
 bool game_over = false;
 
 void emptyBuffer() {
@@ -36,20 +42,28 @@ void emptyBuffer() {
 }
 
 void show_help() {
+    int HelpStop;
     printf("HELP____________\n");
+    printf("Entrez 0 pour retourner au menu principal.\n");
+    scanf(" %d", &HelpStop);
+    while (HelpStop != 0) {
+        emptyBuffer();
+        printf("Erreur, veuillez entrer 0 pour retrouner au menu principal\n");
+        scanf(" %d", &HelpStop);
+    }
 }
 
 
 void grid_initialization() {
-    for (line = 0; line < 9; line++) {                          //grid ini
-        for (column = 0; column < 9; ++column) {
+    for (line = 0; line < 10; line++) {                          //grid ini
+        for (column = 0; column < 10; ++column) {
             grid[line][column] = ' ';
         }
     }
 
 
-    for (line = 0; line < 9; line++) {                          //playersgrid ini
-        for (column = 0; column < 9; ++column) {
+    for (line = 0; line < 10; line++) {                          //playersgrid ini
+        for (column = 0; column < 10; ++column) {
             playerGrid[line][column] = ' ';
         }
 
@@ -124,7 +138,7 @@ void show_player_grid() {
 
 
     printf("||10 ");
-    for (column = 0; column < 10; ++column) { printf("| %c ", grid[9][column]); }
+    for (column = 0; column < 10; ++column) { printf("| %c ", playerGrid[9][column]); }
     printf("||\n");
     for (int horizontal_separation = 0; horizontal_separation < 47; ++horizontal_separation) {
         printf("_");
@@ -163,7 +177,7 @@ void add_boats() {
 }
 
 void Menu() {
-    printf("Bienvenue dans la bataille navale !\n\n"
+    do{printf("Bienvenue dans la bataille navale !\n\n"
            "Entrez 1 pour jouer\n"
            "Entrez 2 pour voir l'aide du jeu\n");
     scanf("%d", &MenuChoice);
@@ -185,7 +199,7 @@ void Menu() {
         default:
             break;
 
-    }
+    }}while (MenuChoice!=1);
 
 
 }
@@ -193,23 +207,27 @@ void Menu() {
 
 void PlayerInput() {
 
+    column_input_letter= 'l';       //resets the inputs so it doesn't take the same as the previous round
     printf("Veuillez choisir une colonne ( de A à J)\n");
     scanf(" %c", &column_input_letter);
     column_input_letter = tolower(column_input_letter);
     while ((column_input_letter - 'a') < 0 || (column_input_letter - 'a' > 9)) {
+        emptyBuffer();
         printf("Veuillez choisir une colonne correcte allant de A à J \n");
         scanf(" %c", &column_input_letter);
     }
-    printf("Colonne choisie : %c\n", toupper(column_input_letter));
+    printf("Colonne choisie : %c\n\n", toupper(column_input_letter));
 
-
+    emptyBuffer();
+    line_input_number=0;
     printf("Veuillez choisir une ligne ( de 1 à 10)\n");
     scanf(" %d", &line_input_number);
-    while (line_input_number < 0 || line_input_number > 10) {
-        printf("Veuillez choisir une line correcte allant de 1 à 10 \n");
+    while (line_input_number < 1 || line_input_number > 10) {
+        emptyBuffer();
+        printf("Veuillez choisir une ligne correcte allant de 1 à 10 \n");
         scanf(" %d", &line_input_number);
     }
-    printf("Ligne choisie : %d\n", line_input_number);
+    printf("Ligne choisie : %d\n\n", line_input_number);
 
 
     playercolumn = column_input_letter - 'a';                   //conversion of the letter to a number
@@ -219,14 +237,23 @@ void PlayerInput() {
         'X') {                //compare user input's location with the hidden grid's value to check the result
         playerGrid[playerline][playercolumn] = 'X';
         printf("touché!\n");
-        if (new_sunk_boat < sunk_boats )        //verification if it's a new sunk boat
+        score = score + 1;
+        grid[playerline][playercolumn] = 'T';
+
+
+        if (new_sunk_boat < sunk_boats)        //verification if it's a new sunk boat
         {
             printf("Coulé!\n");
             new_sunk_boat = new_sunk_boat + 1;
         }
     } else {
-        playerGrid[playerline][playercolumn] = 'O';
-        printf("loupé!\n");
+        if (grid[playerline][playercolumn] == 'T') { printf("Vous avez déjà joué cette case !\n"); }
+
+        if (grid[playerline][playercolumn] == ' ') {
+            printf("loupé!\n");
+            score = score + 1;
+            playerGrid[playerline][playercolumn] = 'O';
+        }
     }
 
     show_player_grid();
@@ -247,6 +274,11 @@ void verification() {
     }
     if (boat1_sunkparts == 5) {
         boat1_sunk = true;
+        if (boat1_msg == false)                       //a condition, so it only displays sunk one time
+        {
+            printf("bato 1 koulé!\n");
+            boat1_msg = true;
+        }
         sunk_boats = sunk_boats + 1;
     }
 
@@ -263,6 +295,10 @@ void verification() {
     if (boat2_sunkparts == 4) {
         boat2_sunk = true;
         sunk_boats = sunk_boats + 1;
+        if (boat2_msg == false) {
+            printf("bato 2 koulé \n");
+            boat2_msg = true;
+        }
     }
 
     int boat3_sunkparts = 0;                     //reset the sunkparts parts before boat3's check
@@ -277,6 +313,10 @@ void verification() {
     if (boat3_sunkparts == 3) {
         boat3_sunk = true;
         sunk_boats = sunk_boats + 1;
+        if (boat3_msg == false) {
+            printf("bato 3 koulé \n");
+            boat3_msg = true;
+        }
     }
 
     int boat4_sunkparts = 0;                     //reset the sunkparts parts before boat4's check
@@ -291,6 +331,10 @@ void verification() {
     if (boat4_sunkparts == 3) {
         boat4_sunk = true;
         sunk_boats = sunk_boats + 1;
+        if (boat4_msg == false) {
+            printf("bato 4 koulé \n");
+            boat4_msg = true;
+        }
     }
 
 
@@ -306,6 +350,11 @@ void verification() {
     if (boat5_sunkparts == 2) {
         boat5_sunk = true;
         sunk_boats = sunk_boats + 1;
+        if (boat5_msg == false) {
+            printf("bato 5 koulé \n");
+            boat5_msg = true;
+        }
+
     }
 
     if (sunk_boats == 5) { game_over = true; }       //checks the game over with a count
@@ -313,20 +362,22 @@ void verification() {
 }
 
 
-void Play(){
+void Play() {
     while (game_over == false) {
         PlayerInput();
         verification();
-        show_grid();
     }
-    printf("GG!");
+    printf("GG!\n");
+    printf("Votre score est de %d.\n", score);
+
 
 }
 
 int main() {
 
-    add_boats();
+    SetConsoleOutputCP(CP_UTF8);
     Menu();
+    add_boats();
     Play();
 
     return 0;
